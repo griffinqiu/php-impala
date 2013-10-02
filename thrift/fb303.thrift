570ad1979852c8aeeade04c2b315e5554a495852
Buck Ryan <fryan@rjmetrics.com>
2013-10-02 13:59:17 -0400

    fix typo in namespace name

--- thrift/fb303.thrift
+++ thrift/fb303.thrift
@@ -24,7 +24,7 @@
 namespace java com.facebook.fb303
 namespace cpp facebook.fb303
 namespace perl Facebook.FB303
-namespace php Faceboox.FB303
+namespace php Facebook.FB303
 
 /**
  * Common status reporting mechanism across all services
